2ed91cf41937f57acddfcdf8c8187c9437489843
Oleg Prokin <prokin.o@soft-logic.ru>
2025-12-24 15:47:46 +0700

    v0.11.1
    - Исправлен подсчет рейтинга

---
+++ src/main/java/ru/softlogic/paylogic_kitchen/entity/Rating.java
@@ -0,0 +1,60 @@
+package ru.softlogic.paylogic_kitchen.entity;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "rating")
+public class Rating {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private Short score;
+
+    @ManyToOne
+    @JoinColumn(name = "restaurant_id", nullable = true)
+    private Restaurant restaurant;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id", nullable = true)
+    private Order order;
+
+    // constructors, getters, setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Short getScore() {
+        return score;
+    }
+
+    public void setScore(Short score) {
+        this.score = score;
+    }
+
+    public Restaurant getRestaurant() {
+        return restaurant;
+    }
+
+    public void setRestaurant(Restaurant restaurant) {
+        this.restaurant = restaurant;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+}
